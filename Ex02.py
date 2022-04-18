70fc701054ce6cda1798574b661b0e7bb63af918
Camila Evangelista <94421173+camilaeva1@users.noreply.github.com>
2022-04-17 21:56:11 -0300

    Update Ex02.py
    Desafio 01 Informar ao uduário que ele deve inserir algo no programa

--- Ex02.py
+++ Ex02.py
@@ -1,8 +1,12 @@
 #Counting the Number of Characters
+#Desafio 01 Informar ao uduário que ele deve inserir algo no programa
 texto = input("What is the input string? ")
 ncaracter = len(texto)
 print(ncaracter)
-print(texto, "has", ncaracter, "caracther.", sep = " ", end = "\n")
+if(ncaracter == 0):
+  print("Digite algo no programa!")
+else:
+  print(texto, "has", ncaracter, "caracther.", sep = " ", end = "\n")
 
 
 
