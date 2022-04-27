07a4d4692681ec7bdc83d92c33716d037e99ac49
Camila Evangelista <94421173+camilaeva1@users.noreply.github.com>
2022-04-27 10:15:01 -0300

    Create Ex21.py
    Feito em sala.

---
+++ Ex21.py
@@ -0,0 +1,3 @@
+ano = ["JANEIRO", "FEVEREIRO", "MARÇO", "ABRIL", "MAIO", "JUNHO", "JULHO", "AGOSTO", "SETEMBRO", "OUTUBRO", "NOVEMBRO", "DEZEMBRO"]
+num = int(input("Qual o número do mês? "))
+print("O nome do mês é {}".format(ano[num-1]))
